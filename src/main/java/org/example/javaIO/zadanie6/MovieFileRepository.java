c4f791f9525555a0879ea75cc103752cc5a6bda1
juliakiczka <juliakiczka838@gmail.com>
2023-09-25 20:59:29 +0200

    adding a part of exercise6

---
+++ src/main/java/org/example/javaIO/zadanie6/MovieFileRepository.java
@@ -0,0 +1,10 @@
+package org.example.javaIO.zadanie6;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MovieFileRepository {
+    private final MovieParser movieParser = new MovieParser();
+    private final static Path PATH = Paths.get("");
+
+}
